c1fb1dc64c6e4ba75a8f31aae781cdb9e043e762
EchauriMu <edechaurimu@ittepic.edu.mx>
2025-02-23 18:40:08 -0700

    v1 produccion

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:82/api/v1",
+  baseURL: "https://notifinance.onrender.com/api/v1",
   withCredentials: true, // Permite el uso de cookies
   headers: {
     "Content-Type": "application/json",
